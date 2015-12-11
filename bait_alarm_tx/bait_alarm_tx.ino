15826476ee78eb52e80d07afa1df33a812adf6a6
nasokst <at.stoianov@gmail.com>
2015-12-11 19:08:39 +0200

    New transmitter code.

--- bait_alarm_tx/bait_alarm_tx.ino
+++ bait_alarm_tx/bait_alarm_tx.ino
@@ -16,11 +16,15 @@
 
 #define TX_PIN PB0  //pin where your transmitter is connected
 //#define LED_PIN PB2 //pin for blinking LED
-#define RUN_INTERRUPT1 PCINT3//on runnn
-#define RUN_INTERRUPT2 PCINT4//on runnn
+#define INTERRUPTPIN1 PCINT3//on runnn
+#define DATADIRECTIONPIN1 DDB3 //Page 64 of data sheet
+#define PORTPIN1 PB3 //Page 64
+#define READPIN1 PINB3 //page 64
 
-#define RUN_PIN1 PB3//on runnn
-#define RUN_PIN2 PB4//on runnn
+#define INTERRUPTPIN2 PCINT4//on runnn
+#define DATADIRECTIONPIN2 DDB4 //Page 64 of data sheet
+#define PORTPIN2 PB4 //Page 64
+#define READPIN2 PINB4 //page 64
 
 //uint8_t data_pb3_L[5] = {78, 64, 115, 0, 10}; // {N, @, s, rand, rand}
 uint8_t data_pb3_H[5] = {78, 64, 115, 0, 30}; // {N, @, s, rand, rand}
@@ -29,7 +33,7 @@ uint8_t data_pb4_H[5] = {78, 64, 115, 1, 31}; // {N, @, s, rand, rand}
 //int value_1 = 0;
 //int value_1_1 = 0;
 //int value_2 = 1;
-int sec_20 = 1;
+static volatile int sec_20 = 1;
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -40,64 +44,37 @@ int sec_20 = 1;
 
 void setup(){
   cli();//disable interrupts during setup
- // pinMode(LED_PIN, OUTPUT);
-  pinMode(PB1, OUTPUT);
-  pinMode(RUN_INTERRUPT1, INPUT);
-  pinMode(RUN_INTERRUPT2, INPUT);  
+//  pinMode(LED_PIN, OUTPUT);
+  //pinMode(PB1, OUTPUT);
+  //pinMode(RUN_INTERRUPT1, INPUT);
+  //pinMode(RUN_INTERRUPT2, INPUT);  
   
   //man.workAround1MhzTinyCore(); //add this in order for transmitter to work with 1Mhz Attiny85/84
   man.setupTransmit(TX_PIN, MAN_1200);  
   
-  sbi(PCMSK, PCINT3); // Which pins are affected by the interrupt
-  sbi(PCMSK, PCINT4); // Which pins are affected by the interrupt
-  sbi(GIMSK, PCIE); // Turn on Pin Change interrupt  
+  PCMSK |= (1 << INTERRUPTPIN1);
+  PCMSK |= (1 << INTERRUPTPIN2);
+  GIMSK |= (1 << PCIE);   // enable PCINT interrupt in the general interrupt mask //SBI
+
+  DDRB &= ~(1 << DATADIRECTIONPIN1); //cbi(DDRB, DATADIRECTIONPIN);//  set up as input  - pinX clear bit  - set to zero
+  PORTB |= (1<< PORTPIN1); //cbi(PORTB, PORTPIN);// disable pull-up. hook up pulldown resistor. - set to zero
+  
+  DDRB &= ~(1 << DATADIRECTIONPIN2); //cbi(DDRB, DATADIRECTIONPIN);//  set up as input  - pinX clear bit  - set to zero
+  PORTB |= (1<< PORTPIN2); //cbi(PORTB, PORTPIN);// disable pull-up. hook up pulldown resistor. - set to zero
+  
+  sei(); //last line of setup - enable interrupts after setup
 }
 
 void loop(){
 
-  value_1 = analogRead(RUN_INTERRUPT1);
-  value_2 = analogRead(RUN_INTERRUPT2);  
-  
   --sec_20;
 
-  if (value_1 > 0)  
-  {
-    sec_20 = 30000;
-      
-    if (value_1 <= 623) {
-      digitalWrite(LED_PIN, HIGH);
-      //delay(200);  // if is in ISR(PCINT0_vect), NOT DELAY
-      digitalWrite(LED_PIN, LOW);
-      
-      man.transmitArray(5, data_pb3_L);
-    }
-    else {
-      digitalWrite(PB1, HIGH);
-      //delay(200); // if is in ISR(PCINT0_vect), NOT DELAY
-      digitalWrite(PB1, LOW);      
-      
-      man.transmitArray(5, data_pb3_H);
-    }
-  }
-      
-  if (value_2 > 0)
-  {
-    sec_20 = 30000;
-    
-    if (value_2 < 623) {
-      man.transmitArray(5, data_pb4_L);
-    }
-    else {
-      man.transmitArray(5, data_pb4_H);
-    }
-  }  
-
-  if (0 >= sec_20) {
-    digitalWrite(LED_PIN, HIGH);
-    digitalWrite(PB1, HIGH);
+   if (0 >= sec_20) {
+    //digitalWrite(LED_PIN, HIGH);
+    //digitalWrite(PB1, HIGH);
     delay(400);
-    digitalWrite(LED_PIN, LOW);
-    digitalWrite(PB1, LOW);    
+    //digitalWrite(LED_PIN, LOW);
+    //digitalWrite(PB1, LOW);    
     
     system_sleep();
   }
@@ -111,5 +88,23 @@ void system_sleep() {
   sbi(ADCSRA,ADEN);  // Switch Analog to Digital converter ON
 }
 
-ISR(PCINT0_vect) {
+ISR(PCINT0_vect)
+{
+  //Since the PCINTn triggers on both rising and falling edge let's just looks for rising edge
+  //i.e. pin goes to 5v
+  byte pinState1, pinState2;
+  pinState1 = (PINB >> READPIN1)& 1; //PINB is the register to read the state of the pins
+  pinState2 = (PINB >> READPIN2)& 1; //PINB is the register to read the state of the pins  
+  
+  if (pinState1 > 0) //look at the pin state on the pin PINB register- returns 1 if high
+  {
+    sec_20 = 30000;
+    man.transmitArray(5, data_pb3_H);
+  }
+ 
+  if (pinState2 > 0) //look at the pin state on the pin PINB register- returns 1 if high
+  {
+    sec_20 = 30000;
+    man.transmitArray(5, data_pb4_H);
+  }  
 }
